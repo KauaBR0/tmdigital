b9074aa5bbe94a875c0ed3d49bc6866b87a16463
Kauan <kaua.sacramento@gmail.com>
2026-01-09 22:36:08 -0300

    feat(frontend): Configure PrimeNG, NgRx, and global styles

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,10 +1,29 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideAnimations(),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }),
+  ],
 };
