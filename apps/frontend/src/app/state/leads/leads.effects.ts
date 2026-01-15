7b6110b2c048eb7f65b0bd215b5803d8dbda2cd7
Kauan <kaua.sacramento@gmail.com>
2026-01-15 13:06:32 -0300

    fix: minor bug in state management

--- apps/frontend/src/app/state/leads/leads.effects.ts
+++ apps/frontend/src/app/state/leads/leads.effects.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { LeadsService } from '../../core/services/leads.service';
 import { LeadsActions } from './leads.actions';
+import { PropertiesActions } from '../../state/properties/properties.actions';
 import { catchError, map, mergeMap, of } from 'rxjs';
 
 @Injectable()
@@ -11,14 +12,19 @@ export class LeadsEffects {
 
   loadLeads$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(LeadsActions.loadLeads),
+      ofType(
+        LeadsActions.loadLeads,
+        PropertiesActions.addPropertySuccess,
+        PropertiesActions.updatePropertySuccess,
+        PropertiesActions.deletePropertySuccess,
+      ),
       mergeMap(() =>
         this.leadsService.getLeads().pipe(
           map((leads) => LeadsActions.loadLeadsSuccess({ leads })),
-          catchError((error) => of(LeadsActions.loadLeadsFailure({ error })))
-        )
-      )
-    )
+          catchError((error) => of(LeadsActions.loadLeadsFailure({ error }))),
+        ),
+      ),
+    ),
   );
 
   addLead$ = createEffect(() =>
@@ -27,10 +33,10 @@ export class LeadsEffects {
       mergeMap(({ lead }) =>
         this.leadsService.createLead(lead).pipe(
           map((newLead) => LeadsActions.addLeadSuccess({ lead: newLead })),
-          catchError((error) => of(LeadsActions.addLeadFailure({ error })))
-        )
-      )
-    )
+          catchError((error) => of(LeadsActions.addLeadFailure({ error }))),
+        ),
+      ),
+    ),
   );
 
   updateLead$ = createEffect(() =>
@@ -38,11 +44,13 @@ export class LeadsEffects {
       ofType(LeadsActions.updateLead),
       mergeMap(({ lead }) =>
         this.leadsService.updateLead(lead).pipe(
-          map((updatedLead) => LeadsActions.updateLeadSuccess({ lead: updatedLead })),
-          catchError((error) => of(LeadsActions.updateLeadFailure({ error })))
-        )
-      )
-    )
+          map((updatedLead) =>
+            LeadsActions.updateLeadSuccess({ lead: updatedLead }),
+          ),
+          catchError((error) => of(LeadsActions.updateLeadFailure({ error }))),
+        ),
+      ),
+    ),
   );
 
   deleteLead$ = createEffect(() =>
@@ -51,9 +59,9 @@ export class LeadsEffects {
       mergeMap(({ id }) =>
         this.leadsService.deleteLead(id).pipe(
           map(() => LeadsActions.deleteLeadSuccess({ id })),
-          catchError((error) => of(LeadsActions.deleteLeadFailure({ error })))
-        )
-      )
-    )
+          catchError((error) => of(LeadsActions.deleteLeadFailure({ error }))),
+        ),
+      ),
+    ),
   );
 }
